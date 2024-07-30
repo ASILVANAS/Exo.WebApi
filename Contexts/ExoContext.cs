d0405b5f65a2f585f5c278f6b11da07413821191
Adelson Silva Nascimento <asilvanas.asn@gmail.com>
2024-07-30 00:17:02 -0300

    Initial commit

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,25 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext : DbContext
+    {
+        public ExoContext()
+        {            
+        }
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options)
+        {            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder OptionsBuilder)
+        {
+            if(!OptionsBuilder.IsConfigured)
+            {
+                OptionsBuilder.UseSqlServer("User ID=sa;Password=12345;Server=localhost;Database=ExoApi;"+"Trusted_Conection=False;");
+            }
+        }
+        public DbSet<Projeto> Projetos { get; set; }
+    }
+}
