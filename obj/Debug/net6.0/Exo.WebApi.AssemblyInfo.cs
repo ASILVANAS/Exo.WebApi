2a8094e5f8949eee15770bdc40c8a25af8ae1cbd
Adelson Silva Nascimento <asilvanas.asn@gmail.com>
2024-08-07 04:57:10 -0300

    CRUD USUARIOS
    Atividade 5

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0405b5f65a2f585f5c278f6b11da07413821191")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddd7ea93cbe7909da35f3ef7c81e20b3bd88602a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
